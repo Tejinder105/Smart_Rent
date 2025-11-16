1fbc0c9a780527681dc18df183230bbeed7d332a
Tejinderp105 <tejinderpalsinghc2@gmail.com>
2025-11-16 12:22:17 +0530

    Refactor code structure for improved readability and maintainability

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
+const { withNativeWind } = require("nativewind/metro");
 
-module.exports = withNativeWind(config, { input: './app/global.css' })
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  return withNativeWind(config, {
+    input: "./app/global.css",
+  });
+})();
